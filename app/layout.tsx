6cef8254995b223b097eab2dc06e7fc618f53f92
Sadlineee <sad1ineee@gmail.com>
2024-12-23 00:43:24 +0500

    unknown

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import StyledComponentsRegistry from '@/lib/registry'
 import Theme from '@/components/Theme'
 import { oswald } from '@/fonts/fonts'
